9b345f5ca73a882233cf1983a6ad7b893dfab918
Khac Hieu Dinh <dinh.khachieu.2000@gmail.com>
2022-06-29 00:35:32 -0400

    Finished I think

--- Lab 0/MEM351_1gpio_interrupt_lab0/MEM351_1gpio_interrupt_lab0.ino
+++ Lab 0/MEM351_1gpio_interrupt_lab0/MEM351_1gpio_interrupt_lab0.ino
@@ -8,22 +8,20 @@ file name: MEM459_1gpio_interrupt
 
 int pinx = 7;   
 volatile int statex = HIGH;
-const long int ON_TIME = 1000000;
+const long int ON_TIME = 1000000; // time in microseconds
 const long int OFF_TIME = 500000;
 int globalCycleCounter = 0;
 int GLOBAL_CYCLE_LIMIT = 10;
-const int GLOBAL_TIME_LIMIT = (ON_TIME + OFF_TIME)*GLOBAL_CYCLE_LIMIT/1000;
-unsigned long tms = 0;
-
-
-
+const int GLOBAL_TIME_LIMIT = (ON_TIME + OFF_TIME)*GLOBAL_CYCLE_LIMIT/1000; // Time microseconds must be converted to miliseconds
+bool Finished = false; // Flag raise when 10 cycle is finished. Essentially disable the loop() function
 
 void setup()
 {
   pinMode(pinx, OUTPUT);
   Timer1.initialize(ON_TIME);   // in micro seconds
-  Timer1.attachInterrupt(gpioInterrupt); // gpioInterrupt to interrupt every 1 second
+  Timer1.attachInterrupt(gpioInterrupt); // gpioInterrupt to interrupt every 1 second (initially, can be changed)
   Serial.begin(115200);
+  Serial.println("Starting Interrupt Sketch ...");
 }
 
 // The interrupt will toggle the states of Pins 7 and blink the LED accordingly
@@ -32,14 +30,18 @@ void gpioInterrupt()  // interrupt service routine
 {
     statex = !statex;  
     digitalWrite(pinx, statex);
-    Timer1.setPeriod(statex ? ON_TIME : OFF_TIME);
+    Timer1.setPeriod(statex ? ON_TIME : OFF_TIME); // Update the interrupt interval every time this subroutine is triggered according the current pin state (High -> 1 sec, Low -> 0.5 sec)
 }
 
 void loop() {
+
+  if (Finished) {
+    return;
+  }
+
   do {
     noInterrupts();
-    tms = millis();
-    Serial.print(tms);
+    Serial.print(millis());
     Serial.print("\t");         // prints a tab
     Serial.println(statex);
   //delay(10);   // this 10 ms delay is to reduce the number of lines to be printed, it can be removed
@@ -47,6 +49,7 @@ void loop() {
   } while(millis() <= GLOBAL_TIME_LIMIT);
 
   noInterrupts();
+  Finished = true;
   statex = LOW;
   digitalWrite(pinx, statex);
 }
